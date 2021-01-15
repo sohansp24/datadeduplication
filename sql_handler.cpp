4f83e26501009a602ad37ff64d53accc0b2b1c56
Sohan Patil <46857626+sohansp24@users.noreply.github.com>
2021-01-15 19:29:51 +0530

    Main SQL Session and SQL Query Handler

---
+++ sql_handler.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<cstring>
+#include<mysql/mysql.h>
+using namespace std;
+MYSQL* connect()
+{
+    MYSQL *connect;
+    connect=mysql_init(NULL);
+    if(!connect)
+    {
+        cout<<"failed"<<endl;
+    }
+    connect = mysql_real_connect(connect, "localhost", "admin", "123456", "sample", 0,NULL,0);
+    if(connect)
+    {
+        cout<<"Connection Sucessfull"<<endl;
+    }
+    return connect;
+}
+MYSQL_RES* execute_query(MYSQL* connect, string query)
+{
+    MYSQL_RES *res_set;
+    char query_arr[query.length()];
+    strcpy(query_arr,query.c_str());
+    mysql_query(connect,query_arr);
+    res_set=mysql_store_result(connect);
+    return res_set;
+}
+void show_result(MYSQL_RES * res_set)
+{
+    
+    int i=0;
+    MYSQL_ROW row;
+    unsigned int numrows = mysql_num_rows(res_set);
+    if (numrows!=0)
+    {
+        while (((row=mysql_fetch_row(res_set)) !=NULL))
+        {
+            cout << row[i] << endl;
+        }
+    }
+    else
+    {
+        cout<<"No entry found"<<endl;
+        return;
+    }
+    //mysql_close (connect);
+}
+int main(int argc, char const *argv[])
+{
+    MYSQL*connect_obj=connect();
+    MYSQL_RES * res_set= execute_query(connect_obj,"select * from test where entry1='sohan';");
+    show_result(res_set);
+    return 0;
+}
