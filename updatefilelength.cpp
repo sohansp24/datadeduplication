e23ada25449335bb8db7d0bcadfb36e89e746f52
Sohan Patil <46857626+sohansp24@users.noreply.github.com>
2021-02-18 18:56:13 +0530

    UpdateFileLength

---
+++ updatefilelength.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include<mysql/mysql.h>
+#include<fstream>
+#include "sql_handler.cpp"
+MYSQL* connect_obj=connect();
+MYSQL_RES* res_set;
+MYSQL_ROW row;
+int i=0;
+void LengthOfOriginalFile(int fileId,string FileName)
+{
+    string location="File/"+FileName;
+    ifstream fin;
+    long filesize=0;
+    if(fin)
+    {
+        fin.open(location);
+        long begin=fin.tellg();
+        fin.seekg(0,ios::end);
+        long end=fin.tellg();
+        filesize=(end-begin)/1024;
+    }
+    string query="update userFile set fileSize= "+to_string(filesize+1) +" where userFileId= "+to_string(fileId)+" ;";
+    execute_query(connect_obj,query);
+    fin.close();
+}
+void LengthOfChunkFile(vector<string> sha256list)
+{
+    ifstream fin;
+    string fileLocation="";
+    long filelen=0;
+    for (string i:sha256list)
+    {
+        fileLocation="chunks/"+i;
+        fin.open(fileLocation);
+        if (fin)
+        {
+            long begin=fin.tellg();
+            fin.seekg(0,ios::end);
+            long end=fin.tellg();
+            filelen=(end-begin)/1024;
+            string query="update hashTable set chunkSize= "+to_string(filelen+1)+ " where sha256= "+i+" ;";
+        }
+        else
+            cout<<"File not found"<<endl;
+        fin.close();
+    }
+}
+/*
+int main()
+{
+    LengthOfOriginalFile(1,"ff");
+}*/
