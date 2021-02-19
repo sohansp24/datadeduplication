c48246d63852a2327f38199c21270c6f4379e45d
Sohan Patil <46857626+sohansp24@users.noreply.github.com>
2021-02-19 18:28:59 +0530

    Download File module

---
+++ downloadFile.h
@@ -0,0 +1,32 @@
+#ifndef DOWNLOAD_FILE_H
+#define DOWNLOAD_FILE_H
+#include<bits/stdc++.h>
+#include "file_opr.h"
+#include "retrieveID.h"
+void DownloadFile(int userId)
+{
+    cout<<"---------------------------Download File---------------------------"<<endl;
+    cout<<"Files you have:- "<<endl;
+    int fileId=showFiles(userId);
+    string filename=showFilesName(fileId);
+    vector <int> shaId=retriveshaID(fileId);
+    vector<string> sha256=retrieveSha(shaId);
+    vector<string> changename;
+    char *token = strtok((char*)filename.c_str(), "[_.]"); 
+    changename.push_back((string)token);
+    while (token != NULL) 
+    { 
+        changename.push_back((string)token);
+        token = strtok(NULL, "-"); 
+    }  
+    int len=changename.size()-1;
+    filename=changename.at(0)+"(v"+to_string(getVersionNo(fileId))+")."+changename.at(len);
+    createOriginal(sha256,filename);
+
+}
+/*
+int main()
+{
+    DownloadFile(1);
+}*/
+#endif
