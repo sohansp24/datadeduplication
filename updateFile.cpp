baf93249e8e21744bf3b3d4daae1c1ed908cd1df
Sohan Patil <46857626+sohansp24@users.noreply.github.com>
2021-03-16 09:57:43 +0530

    Update File Module

---
+++ updateFile.cpp
@@ -0,0 +1,71 @@
+#ifndef UPLOAD_FILE
+#define UPLOAD_FILE
+#include<bits/stdc++.h>
+#include<ctime>
+#include<vector>
+#include "sql_handler.h"
+#include "createChunk.h"
+#include "updatefilelength.h"
+#include "uploadFile.h"
+#include "retrieveID.h"
+namespace mysql_updatefile
+{
+    MYSQL * connect_obj=connect();
+    MYSQL_RES * res_set;
+    MYSQL_ROW row;
+}
+using namespace std;
+
+bool checkFileName(int userId,string filename)
+{
+    string query="select * from userFile where userId= "+to_string(userId)+" and fileName= '"+filename+"' ;";
+    mysql_updatefile::res_set=execute_query(mysql_updatefile::connect_obj,query);
+    unsigned int numrows = mysql_num_rows(mysql_updatefile::res_set);
+    if (numrows!=0)
+        return true;
+    else
+        return false;
+}
+
+void update(int userId)
+{
+    int versionCount=0;
+    cout<<"Enter name of the file : ";
+    string filename;
+    cin>>filename;
+    cout<<endl;
+    int fileId=retrieveFileId(userId,filename);
+    int versionNo=retrieveVersionNo(fileId);
+    string newFileLocation="File/"+filename;
+    if(checkFileName(userId,filename))
+    {
+        time_t now=time(0);
+        tm *ltm=localtime(&now);
+        string current_datentime=to_string(ltm->tm_mday)+"-"+to_string(1+ltm->tm_mon) +"-"+to_string(1900+ltm->tm_year)+"|"+to_string(5+ltm->tm_hour)+":"+to_string(30+ ltm->tm_hour)+":"+to_string(ltm->tm_sec);
+        vector<string> tokens;
+        stringstream line(filename);
+        string intermediate;
+        while(getline(line,intermediate,'\\.'))
+            tokens.push_back(intermediate);
+        string newFileName=tokens.at(0)+"_"+current_datentime+"."+tokens.at(1);
+        versionCount=versionNo+1;
+        string query="insert into userFile (userId,fileName,versionNo,versionOf) values ("+to_string(userId)+",'"+newFileName+"',"+to_string(versionCount)+","+to_string(fileId)+");";
+        execute_query(mysql_updatefile::connect_obj,query);
+        int versionId=retrieveVersionId(fileId,versionCount);
+        createChunk(versionId,newFileLocation);
+        cout<<versionId<<endl;
+        insertVersionFileTable(versionId);
+        LengthOfOriginalFile(versionId,filename);
+        cout<<versionId<<endl;
+        vector <string> shaValue=retrieveShaValue(versionId);
+        LengthOfChunkFile(shaValue);
+    }
+    else
+        cout<<"File not found"<<endl;
+}
+int main()
+{
+    update(1);
+    return 0;
+}
+#endif
