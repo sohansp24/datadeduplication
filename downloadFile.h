6736a97b38c35fedee0ff9f698784667abd8af12
Sohan Patil <46857626+sohansp24@users.noreply.github.com>
2021-04-01 11:14:11 +0530

    Improved show files module

--- downloadFile.h
+++ downloadFile.h
@@ -8,21 +8,24 @@ void DownloadFile(int userId)
     cout<<"---------------------------Download File---------------------------"<<endl;
     cout<<"Files you have:- "<<endl;
     int fileId=showFiles(userId);
-    string filename=showFilesName(fileId);
-    vector <int> shaId=retriveshaID(fileId);
-    vector<string> sha256=retrieveSha(shaId);
-    vector<string> changename;
-    char *token = strtok((char*)filename.c_str(), "[_.]"); 
-    changename.push_back((string)token);
-    while (token != NULL) 
-    { 
+    if(fileId!=-1)
+    {
+        string filename=showFilesName(fileId);
+        vector <int> shaId=retriveshaID(fileId);
+        vector<string> sha256=retrieveSha(shaId);
+        vector<string> changename;
+        char *token = strtok((char*)filename.c_str(), "[_.]"); 
         changename.push_back((string)token);
-        token = strtok(NULL, "-"); 
-    }  
-    int len=changename.size()-1;
-    filename=changename.at(0)+"(v"+to_string(getVersionNo(fileId))+")."+changename.at(len);
-    createOriginal(sha256,filename);
-
+        while (token != NULL) 
+        { 
+            changename.push_back((string)token);
+            token = strtok(NULL, "-"); 
+        }  
+        int len=changename.size()-1;
+        filename=changename.at(0)+"(v"+to_string(getVersionNo(fileId))+")."+changename.at(len);
+        createOriginal(sha256,filename);
+        cout<<"File downloaded sucessfully.."<<endl;
+    }
 }
 /*
 int main()
